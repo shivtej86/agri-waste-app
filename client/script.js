faccb4cb46ac86590cc49f76a05ffb76adec40cb
shivtej86 <shivtejghorpade0@gmail.com>
2026-01-08 01:04:40 +0530

    Updated API URL

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api'; 
+const API_URL = 'https://agri-waste-api.onrender.com'; 
 let token = localStorage.getItem('token');
 let currentUser = JSON.parse(localStorage.getItem('user'));
 
